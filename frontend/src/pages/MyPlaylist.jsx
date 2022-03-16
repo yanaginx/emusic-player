fea5f8deae0a7c936918e1404f70beecda27b79e
Duong Van Chan <duongchan159@gmail.com>
2022-03-16 20:08:44 +0700

    Added fetch all playlists method on my playlist

--- frontend/src/pages/MyPlaylist.jsx
+++ frontend/src/pages/MyPlaylist.jsx
@@ -24,6 +24,12 @@ function MyPlaylist({ auth }) {
     // setPlayingTrack(track);
   }
 
+  // DEBUG
+  useEffect(() => {
+    console.log("[DEBUG] current playlistResults: ", playlistResults);
+  }, [playlistResults]);
+  // END : DEBUG
+
   useEffect(() => {
     if (!user_auth) return;
     spotifyApi.setAccessToken(user_auth);
@@ -58,43 +64,119 @@ function MyPlaylist({ auth }) {
     if (!user_auth) return;
     if (!myInfo) return;
 
-    // console.log("[DEBUG] my info: ", myInfo);
-    spotifyApi.getUserPlaylists(myInfo.id).then(
-      (res) => {
-        console.log("[DEBUG] playlist results: ", res.body);
-        setPlaylistResults(
-          res.body.items.map((playlist) => {
-            let smallestAlbumImage = "";
-            if (playlist.images.length > 0) {
-              smallestAlbumImage = playlist.images.reduce((smallest, image) => {
-                if (image.height < smallest.height) return image;
-                return smallest;
-              });
-            }
-            return {
-              owner: playlist.owner.display_name,
-              name: playlist.name,
-              uri: playlist.uri,
-              albumUrl: smallestAlbumImage.url,
-            };
-          })
-        );
-        // console.log("[DEBUG] playlist results: ", res.body);
-      },
-      (err) => {
-        console.log("[DEBUG] error in search: ", err);
-        console.log("[DEBUG] error in search: ", typeof err);
-        if (
-          err.toString().search("No token provided") !== -1 ||
-          err.toString().search("The access token expired") !== -1
+    const fetchAllPlaylists = async () => {
+      const allPlaylists = (await spotifyApi.getUserPlaylists(myInfo.id)).body;
+
+      // Fetch all available playlists
+      if (allPlaylists.total > allPlaylists.limit) {
+        // Divide the total number of playlist by the limit to get the number of API calls
+        for (
+          let i = 1;
+          i < Math.ceil(allPlaylists.total / allPlaylists.limit);
+          i++
         ) {
-          console.log("Refreshed here from Search box");
-          dispatch(refreshAuthToken());
+          const playlistToAdd = (
+            await spotifyApi.getUserPlaylists(myInfo.id, {
+              limit: allPlaylists.limit,
+              offset: i * allPlaylists.limit,
+            })
+          ).body;
+
+          playlistToAdd.items.forEach((item) => allPlaylists.items.push(item));
         }
       }
-    );
-    return () => {};
+      // console.log("[DEBUG] all playlists: ", allPlaylists);
+      // setPlaylistResults(allPlaylists);
+      setPlaylistResults(
+        allPlaylists.items.map((playlist) => {
+          let smallestAlbumImage = "";
+          if (playlist.images.length > 0) {
+            smallestAlbumImage = playlist.images.reduce((smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            });
+          }
+          return {
+            owner: playlist.owner.display_name,
+            name: playlist.name,
+            uri: playlist.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+      // console.log("[DEBUG] playlist results: ", res.body);
+    };
+
+    fetchAllPlaylists().catch((err) => {
+      console.log("[DEBUG] error in search: ", err);
+      console.log("[DEBUG] error in search: ", typeof err);
+      if (
+        err.toString().search("No token provided") !== -1 ||
+        err.toString().search("The access token expired") !== -1
+      ) {
+        console.log("Refreshed here from Search box");
+        dispatch(refreshAuthToken());
+      }
+    });
   }, [user_auth, myInfo, dispatch]);
+
+  // useEffect(() => {
+  //   if (!user_auth) return;
+  //   if (!myInfo) return;
+
+  //   // console.log("[DEBUG] my info: ", myInfo);
+  //   spotifyApi.getUserPlaylists(myInfo.id).then(
+  //     (res) => {
+  //       console.log("[DEBUG] playlist results: ", res.body);
+  //       const allPlaylists = [...res.body.items];
+  //       // Fetch all available playlists
+  //       if (res.body.total > res.body.limit) {
+  //         // Divide the total number of playlist by the limit to get the number of API calls
+  //         for (let i = 1; i < Math.ceil(res.body.total / res.body.limit); i++) {
+  //           spotifyApi
+  //             .getUserPlaylists(myInfo.id, {
+  //               limit: res.body.limit,
+  //               offset: i * res.body.limit,
+  //             })
+  //             .then((data) => {
+  //               data.body.items.forEach((item) => allPlaylists.push(item));
+  //             });
+  //         }
+  //       }
+  //       console.log("[DEBUG] all playlists: ", allPlaylists);
+  //       setPlaylistResults(
+  //         allPlaylists.map((playlist) => {
+  //           let smallestAlbumImage = "";
+  //           if (playlist.images.length > 0) {
+  //             smallestAlbumImage = playlist.images.reduce((smallest, image) => {
+  //               if (image.height < smallest.height) return image;
+  //               return smallest;
+  //             });
+  //           }
+  //           return {
+  //             owner: playlist.owner.display_name,
+  //             name: playlist.name,
+  //             uri: playlist.uri,
+  //             albumUrl: smallestAlbumImage.url,
+  //           };
+  //         })
+  //       );
+  //       // console.log("[DEBUG] playlist results: ", res.body);
+  //     },
+  //     (err) => {
+  //       console.log("[DEBUG] error in search: ", err);
+  //       console.log("[DEBUG] error in search: ", typeof err);
+  //       if (
+  //         err.toString().search("No token provided") !== -1 ||
+  //         err.toString().search("The access token expired") !== -1
+  //       ) {
+  //         console.log("Refreshed here from Search box");
+  //         dispatch(refreshAuthToken());
+  //       }
+  //     }
+  //   );
+  //   return () => {};
+  // }, [user_auth, myInfo, dispatch]);
 
   return (
     <>
